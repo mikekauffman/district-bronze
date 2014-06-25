83ded04819e018531095690cb604c30066562175
mikekauffman <mike.a.kauffman@gmail.com>
2014-06-25 12:42:16 -0600

    fixed spec expectations

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -4,6 +4,6 @@ feature 'User visits the homepage' do
   scenario 'User is welcomed and company name is displayed on homepage' do
     visit '/'
     expect(page).to have_content 'District Bronze'
-    expect(page).to have_content "DC's best organic spray tan"
+    expect(page).to have_content 'Organic Airbrush Tanning'
   end
 end
